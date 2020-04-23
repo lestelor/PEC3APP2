1c778adc9985d14804ea5c95a5dd36f1eee4cecd
lestelor <claudi.fauli@gmail.com>
2020-04-23 14:07:53 +0200

    final version

--- app/src/main/java/edu/uoc/android/SettingsActivity.kt
+++ app/src/main/java/edu/uoc/android/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package edu.uoc.android
 
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -10,6 +12,8 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_settings.*
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -18,11 +22,8 @@ import java.io.FileOutputStream
 
 class SettingsActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
-    val REQUEST_TAKE_PHOTO = 1
     val folder = "UOCImageAPP/"
     val imageTheFile = "imageApp.jpg"
-    var currentPhotoPath: String? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +31,11 @@ class SettingsActivity : AppCompatActivity() {
 
         val imgFile = File(getStorageDir() + File.separator + imageTheFile)
 
-        if (imgFile.exists()) {
+        if (checkPermissions() && imgFile.exists()) {
             val myBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
             ivSetting.setImageBitmap(myBitmap)
         } else {
-            tvSetting.text = "No hay imagen disponible"
+            tvSetting.text = getString(R.string.noImage)
         }
 
         fabuttonSettings?.setOnClickListener { dispatchTakePictureIntent() }
@@ -64,13 +65,14 @@ class SettingsActivity : AppCompatActivity() {
             val fos = FileOutputStream(imageFile)
             fos.write(stream.toByteArray())
             fos.close()
-            Toast.makeText(applicationContext,"Imagen guardada",Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext,getString(R.string.pictureSaved),Toast.LENGTH_SHORT).show()
             tvSetting.text = ""
         }
     }
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
             takePictureIntent.resolveActivity(packageManager)?.also {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             }
@@ -82,6 +84,31 @@ class SettingsActivity : AppCompatActivity() {
         return getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.toString() + File.separator + folder
     }
 
+    private fun checkPermissions(): Boolean {
+        return if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            true
+        } else {
+            requestPermissions()
+            false
+        }
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_EXTERNAL_STORAGE
+        )
+    }
+
+
+
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+    }
 
 }
 
